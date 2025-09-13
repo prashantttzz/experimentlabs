506115a00491cbca686045e0cd3dc78c8a50632c
prashantttzz <contact.prashant31@gmail.com>
2025-09-14 01:50:00 +0530

    push src/dist

--- backend/dist/src/routes/route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import goalroutes from "./goal.js";
-import authroutes from "./auth.js";
-const router = Router();
-router.use("/goal", goalroutes);
-router.use("/auth", authroutes);
-export default router;
