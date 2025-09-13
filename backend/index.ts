827d93cfb23b73720580e2429f237d9c5dba1b9f
prashantttzz <contact.prashant31@gmail.com>
2025-09-14 02:31:43 +0530

    cors error fixed

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
 app.use(
   cors({
-    origin: "https://experimentlabs.vercel.app/",
+    origin: "https://experimentlabs.vercel.app",
     credentials: true,
   })
 );
